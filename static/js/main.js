83d0abaff36e220af8579eaafff3e2305f1a25b8
vladtsap <vldzpp@gmail.com>
2022-03-19 16:21:56 +0200

    move isMobile() under showTooltipOnClickOnMobile()

--- static/js/main.js
+++ static/js/main.js
@@ -1,23 +1,3 @@
-function isMobile() {
-    const userAgent = navigator.userAgent || navigator.vendor || window.opera;
-
-    // Windows Phone must come first because its UA also contains "Android"
-    if (/windows phone/i.test(userAgent)) {
-        return true;
-    }
-
-    if (/android/i.test(userAgent)) {
-        return true;
-    }
-
-    // iOS detection from: http://stackoverflow.com/a/9039885/177710
-    if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
-        return true;
-    }
-
-    return false;
-}
-
 function initializeThemeSwitcher() {
     const themeSwitch = document.querySelector('.theme-switcher input[type="checkbox"]');
 
@@ -111,6 +91,27 @@ function useSmartTooltipPositioning() {
 }
 
 function showTooltipOnClickOnMobile() {
+    function isMobile() {
+        const userAgent = navigator.userAgent || navigator.vendor || window.opera;
+
+        // Windows Phone must come first because its UA also contains "Android"
+        if (/windows phone/i.test(userAgent)) {
+            return true;
+        }
+
+        if (/android/i.test(userAgent)) {
+            return true;
+        }
+
+        // iOS detection from: http://stackoverflow.com/a/9039885/177710
+        if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
+            return true;
+        }
+
+        return false;
+    }
+
+    
     if (isMobile()) {
         let articleLinks = document.querySelectorAll(".article-link");
         for (let i = 0; i < articleLinks.length; i++) {
